3be0fa3ad15f053d668c9a73541eb17aae398eb8
Catalina Williams <catalina.rose1361@gmail.com>
2019-11-23 14:27:09 -0700

    working js to save text

--- assets/jquery/function.js
+++ assets/jquery/function.js
@@ -1,32 +1,24 @@
-// Clicking a timeblock's "Save" button stores the input text in local storage, allowing the text to persist when the application is refreshed.
-
-// The current day is displayed at the top of the calendar.
-
-// Each timeblock is color coded to indicate whether it is in a past, present, or future hour.
-
-// GIVEN that an employee adds events to a specific hour in a calendar
-
-// WHEN the employee clicks the save button
-
-// THEN events are saved in the timeblock for that hour
-
-
-// Step 1. display current day at the top of the calendar
-    // 
-// Step 2. color time blocks to indicate past present and future
-// Step 3. Add text to chosen block and save in local storage 
-// Step 4. display input in block even after refresh  
-
 $(document).ready(function(){
     
     var dateTime = moment().format("MM-DD-YYYY");
     $("#currentDay").append(dateTime);
     
+    // $(".saveButton").click(function(){
+    //     $("input").value();
+    // })
     
+    // console.log("date is" + dateTime);
+    $(".saveButton").on("click", function(e) {
+        e.preventDefault();
+        var myText = $("#myText").val();
+        console.log("text val" + myText);
+
+        localStorage.setItem("input", myText);
+    });
+    var savedInfo = localStorage.getItem("input");
+    $("#myText").append(savedInfo);
 });
-
-
-
-
-
+// if the time slot has passed them the time slot will be gray
+// if the time slot is current the time slot will be red
+// if the time slot is in the future it will be green
 
